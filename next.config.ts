67ae058d85c04fce0aa7666baeb20d40a168884a
cat terracat <terakot2022@gmail.com>
2025-09-22 03:27:05 +0000

    над срочными рвботами поставь эту картинку https://s1.hostingkartinok.co

--- next.config.ts
+++ next.config.ts
@@ -64,6 +64,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 's1.hostingkartinok.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
