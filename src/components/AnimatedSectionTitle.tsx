9120c00527220b26ac03c5ad52b08b4f8a80edb9
cat terracat <terakot2022@gmail.com>
2025-09-22 04:15:58 +0000

    строго обавь только эти  🛤️🌲🌳🌿☘️🌹🌺🌻🏡🌼🌷🍀🍃🍂🍁🌇🌆

--- src/components/AnimatedSectionTitle.tsx
+++ src/components/AnimatedSectionTitle.tsx
@@ -10,7 +10,7 @@ import React, { useState, useEffect } from 'react';
 const CYCLES_PER_LETTER = 2;
 const SHUFFLE_TIME = 50;
 
-const CHARS = "🗺️📍🏗️📐📏🧭🏞️⛰️🏡🏢📸📹🌸🌻🌷🌿🌲🌳🌾🏠";
+const CHARS = "🛤️🌲🌳🌿☘️🌹🌺🌻🏡🌼🌷🍀🍃🍂🍁🌇🌆";
 
 const ScrambleText = ({ children, className }: {children: string, className?: string}) => {
   const ref = useRef<HTMLSpanElement>(null);
