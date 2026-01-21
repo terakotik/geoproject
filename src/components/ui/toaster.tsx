9f8aa4ff8153d227d18f773814b24315d9cb1990
cat terracat <terakot2022@gmail.com>
2026-01-21 03:56:51 +0000

    окно сделай меньше и пусть вылетает слева а не справа

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -14,7 +14,7 @@ export function Toaster() {
   const { toasts } = useToast()
 
   return (
-    <ToastProvider>
+    <ToastProvider swipeDirection="left">
       {toasts.map(function ({ id, title, description, action, ...props }) {
         return (
           <Toast key={id} {...props}>
