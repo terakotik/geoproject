97fa7c1cedff69c72bb785196728a0a1fcd6e0d2
cat terracat <terakot2022@gmail.com>
2026-02-04 04:33:50 +0000

    установи код счетчика <!-- Yandex.Metrika counter --> <script type="text

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import { GpsJammingToast } from '@/components/GpsJammingToast';
 import CallMeBackButton from '@/components/CallMeBackButton';
 import { ContactDialogProvider } from '@/hooks/use-contact-dialog.tsx';
 import { PT_Sans } from 'next/font/google';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'ГЕОСТРОЙПРОЕКТ - Кадастровые работы и геодезические услуги в Санкт-Петербурге',
@@ -29,8 +30,35 @@ export default function RootLayout({
   return (
     <html lang="ru" suppressHydrationWarning>
       <head>
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=106619282', 'ym');
+
+            ym(106619282, 'init', {
+                ssr:true, 
+                webvisor:true, 
+                clickmap:true, 
+                ecommerce:"dataLayer", 
+                referrer: document.referrer, 
+                url: location.href, 
+                accurateTrackBounce:true, 
+                trackLinks:true
+            });
+          `}
+        </Script>
       </head>
       <body className={cn("font-body antialiased", ptSans.variable)}>
+          <noscript>
+            <div>
+              <img src="https://mc.yandex.ru/watch/106619282" style={{ position: 'absolute', left: '-9999px' }} alt="" />
+            </div>
+          </noscript>
+          
           <ContactDialogProvider>
             <CallMeBackButton />
             <div vaul-drawer-wrapper="" className="min-h-screen flex flex-col">
